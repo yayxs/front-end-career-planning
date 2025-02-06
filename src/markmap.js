49dfec11efa2b85e0bc87a080b5e6beefe796adb
yayxs <leaderywl@163.com>
2025-02-06 15:58:01 +0800

    refactor: migrate project to Next.js with TypeScript

--- src/markmap.js
+++
@@ -1,8 +0,0 @@
-import { loadCSS, loadJS } from 'markmap-common'
-import { Transformer } from 'markmap-lib'
-import * as markmap from 'markmap-view'
-
-export const transformer = new Transformer()
-const { scripts, styles } = transformer.getAssets()
-loadCSS(styles)
-loadJS(scripts, { getMarkmap: () => markmap })
